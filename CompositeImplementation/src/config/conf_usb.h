4f44d31169d7678f5d6edfb5691cb0ad48a2ea9c
t0ll1v3r <jackson.clary@c2mg.org>
2025-06-04 09:01:04 -0700

    Simplified formatting while still developing, will reintroduce fancy stuff once it actually works

--- CompositeImplementation/src/config/conf_usb.h
+++ CompositeImplementation/src/config/conf_usb.h
@@ -39,55 +39,100 @@
 
 #include "compiler.h"
 
+/**
+ * USB Device Configuration
+ * @{
+ */
 #ifndef USB_PID_ATMEL_ASF_HIDCOMPOSITE
-#	define USB_PID_ATMEL_ASF_HIDCOMPOSITE 0x2133
+#  define USB_PID_ATMEL_ASF_HIDCOMPOSITE 0x2133
 #endif
 
-
-/* ------------------------------------------------------------------------
- * ------------------------ Device-wide identifiers -----------------------
- * ----------------------------------------------------------------------*/
+//! Device definition (mandatory)
 #define  USB_DEVICE_VENDOR_ID             USB_VID_ATMEL
 #define  USB_DEVICE_PRODUCT_ID            USB_PID_ATMEL_ASF_HIDCOMPOSITE
 #define  USB_DEVICE_MAJOR_VERSION         1
 #define  USB_DEVICE_MINOR_VERSION         0
-#define  USB_DEVICE_POWER                 100 // VBUS line consumption (mA)
+#define  USB_DEVICE_POWER                 100 // Consumption on VBUS line (mA)
 #define  USB_DEVICE_ATTR                  \
 		(USB_CONFIG_ATTR_REMOTE_WAKEUP|USB_CONFIG_ATTR_BUS_POWERED)
+// (USB_CONFIG_ATTR_SELF_POWERED)
+// (USB_CONFIG_ATTR_BUS_POWERED)
+// (USB_CONFIG_ATTR_REMOTE_WAKEUP|USB_CONFIG_ATTR_SELF_POWERED)
 
 
-/* ------------------------------------------------------------------------
- * --------------------- USB Device string definitions --------------------
- * ----------------------------------------------------------------------*/
+//! USB Device string definitions (Optional)
 #define  USB_DEVICE_MANUFACTURE_NAME      "ATMEL ASF"
 #define  USB_DEVICE_PRODUCT_NAME          "Composite HID Keyboard + Joystick"
+// #define  USB_DEVICE_SERIAL_NAME           "12...EF" // Disk SN for MSC
 
+/**
+ * Device speeds support
+ * @{
+ */
+//! To define a Low speed device
+//#define  USB_DEVICE_LOW_SPEED
 
-/* ------------------------------------------------------------------------
- * ------------------------ Device speeds support -------------------------
- * ----------------------------------------------------------------------*/
+//! To authorize the High speed
 #if (UC3A3||UC3A4)
+//#define  USB_DEVICE_HS_SUPPORT
 #elif (SAM3XA||SAM3U)
 #endif
+//@}
 
-
-/* ------------------------------------------------------------------------
- * ------------------- USB Device Callback definitions --------------------
- * ----------------------------------------------------------------------*/
+/**
+ * USB Device Callbacks definitions (Optional)
+ * @{
+ */
 #define  UDC_VBUS_EVENT(b_vbus_high)
-#define  UDC_SOF_EVENT()            main_sof_action()
-#define  UDC_SUSPEND_EVENT()        main_suspend_action()
-#define  UDC_RESUME_EVENT()         main_resume_action()
-#define  UDC_REMOTEWAKEUP_ENABLE()  main_remotewakeup_enable()
-#define  UDC_REMOTEWAKEUP_DISABLE() main_remotewakeup_disable()
+#define  UDC_SOF_EVENT()                  main_sof_action()
+#define  UDC_SUSPEND_EVENT()              main_suspend_action()
+#define  UDC_RESUME_EVENT()               main_resume_action()
+#define  UDC_REMOTEWAKEUP_ENABLE()        main_remotewakeup_enable()
+#define  UDC_REMOTEWAKEUP_DISABLE()       main_remotewakeup_disable()
+//@}
+
+/**
+ * USB Device low level configuration
+ * When only one interface is used, these configurations are defined by the class module.
+ * For composite device, these configuration must be defined here
+ * @{
+ */
+//! Control endpoint size
+#define  USB_DEVICE_EP_CTRL_SIZE       8
+
+//! Number of interfaces for this device
+#define  USB_DEVICE_NB_INTERFACE       2 // 1 or more
+
+//! Total endpoint used by all interfaces
+//! Note:
+//! It is possible to define an IN and OUT endpoints with the same number on XMEGA product only
+//! E.g. MSC class can be have IN endpoint 0x81 and OUT endpoint 0x01
+#define  USB_DEVICE_MAX_EP             2 // 0 to max endpoint requested by interfaces
+//@}
+
+//@}
 
 
-/* ------------------------------------------------------------------------
- * -------------------  HID-KEYBOARD interface settings -------------------
- * ----------------------------------------------------------------------*/
-#define UDI_HID_KBD_ENABLE_EXT()      main_kbd_enable()
-#define UDI_HID_KBD_DISABLE_EXT()     main_kbd_disable()
-#define UDI_HID_KBD_CHANGE_LED(value) BD76319_ui_kbd_led(value)
+/**
+ * USB Interface Configuration
+ * @{
+ */
+
+
+/**
+ * Configuration of HID Keyboard interface (if used)
+ * @{
+ */
+//! Interface callback definition
+#define  UDI_HID_KBD_ENABLE_EXT()       main_kbd_enable()
+#define  UDI_HID_KBD_DISABLE_EXT()      main_kbd_disable()
+// #define UDI_HID_KBD_ENABLE_EXT() my_callback_keyboard_enable()
+// extern bool my_callback_keyboard_enable(void);
+// #define UDI_HID_KBD_DISABLE_EXT() my_callback_keyboard_disable()
+// extern void my_callback_keyboard_disable(void);
+#define  UDI_HID_KBD_CHANGE_LED(value)  BD76319_ui_kbd_led(value)
+// #define  UDI_HID_KBD_CHANGE_LED(value) my_callback_keyboard_led(value)
+// extern void my_callback_keyboard_led(uint8_t value)
 
 /**
  * USB HID Keyboard low level configuration
@@ -95,66 +140,101 @@
  * For composite device, these configuration must be defined here
  * @{
  */
-// //! Endpoint numbers definition
+//! Endpoint numbers definition
 #define  UDI_HID_KBD_EP_IN           (1 | USB_EP_DIR_IN)
 
-// //! Interface number
+//! Interface number
 #define  UDI_HID_KBD_IFACE_NUMBER    0
+//@}
+//@}
 
-		
-/* ------------------------------------------------------------------------
- * -------------------  HID-JOYSTICK interface settings -------------------
- * ----------------------------------------------------------------------*/
-#define UDI_HID_GENERIC_ENABLE_EXT()  main_generic_enable()
-#define UDI_HID_GENERIC_DISABLE_EXT() main_generic_disable()
 
-//! Sizes of I/O reports
+/**
+ * Configuration of HID Generic interface (if used)
+ * @{
+ */
+//! Interface callback definition
+#define  UDI_HID_GENERIC_ENABLE_EXT()        main_generic_enable()
+#define  UDI_HID_GENERIC_DISABLE_EXT()       main_generic_disable()
+// #define  UDI_HID_GENERIC_REPORT_OUT(ptr)
+// #define  UDI_HID_GENERIC_SET_FEATURE(f)
+/*
+ * #define UDI_HID_GENERIC_ENABLE_EXT() my_callback_generic_enable()
+ * extern bool my_callback_generic_enable(void);
+ * #define UDI_HID_GENERIC_DISABLE_EXT() my_callback_generic_disable()
+ * extern void my_callback_generic_disable(void);
+ * #define  UDI_HID_GENERIC_REPORT_OUT(ptr) my_callback_generic_report_out(ptr)
+ * extern void my_callback_generic_report_out(uint8_t *report);
+ * #define  UDI_HID_GENERIC_SET_FEATURE(f) my_callback_generic_set_feature(f)
+ * extern void my_callback_generic_set_feature(uint8_t *report_feature);
+ */
 #define  UDI_HID_REPORT_IN_SIZE             2
 #define  UDI_HID_REPORT_OUT_SIZE            0
 #define  UDI_HID_REPORT_FEATURE_SIZE        0
+#define  UDI_HID_GENERIC_EP_SIZE            8
 
-// // ! Sizes of I/O endpoints
-// #define  UDI_HID_GENERIC_EP_SIZE            8
-
+/**
+ * USB HID Generic low level configuration
+ * In standalone these configurations are defined by the HID generic module.
+ * For composite device, these configuration must be defined here
+ * @{
+ */
 //! Endpoint numbers definition
+// #define  UDI_HID_GENERIC_EP_OUT   (2 | USB_EP_DIR_OUT)
 #define  UDI_HID_GENERIC_EP_IN    (2 | USB_EP_DIR_IN)
 
 //! Interface number
 #define  UDI_HID_GENERIC_IFACE_NUMBER    1
+//@}
+//@}
 
 
-/* ------------------------------------------------------------------------
- * -------------------------  HID-COMPOSITE stuff -------------------------
- * ----------------------------------------------------------------------*/
+/**
+ * Description of Composite Device
+ * @{
+ */
+//! USB Interfaces descriptor structure
 #define UDI_COMPOSITE_DESC_T \
-	udi_hid_kbd_desc_t udi_hid_kbd; \
-	udi_hid_generic_desc_t udi_hid_generic;
+		udi_hid_kbd_desc_t udi_hid_kbd; \
+		udi_hid_generic_desc_t udi_hid_generic;
 
 #define UDI_COMPOSITE_DESC_FS \
-	.udi_hid_kbd = UDI_HID_KBD_DESC, \
-	.udi_hid_generic = UDI_HID_GENERIC_DESC
+		.udi_hid_kbd = UDI_HID_KBD_DESC, \
+		.udi_hid_generic = UDI_HID_GENERIC_DESC
 
 #define UDI_COMPOSITE_DESC_HS \
-	.udi_hid_kbd = UDI_HID_KBD_DESC, \
-	.udi_hid_generic = UDI_HID_GENERIC_DESC
+		.udi_hid_kbd = UDI_HID_KBD_DESC, \
+		.udi_hid_generic = UDI_HID_GENERIC_DESC
 
 #define UDI_COMPOSITE_API \
-	&udi_api_hid_kbd, \
-	&udi_api_hid_generic
+		&udi_api_hid_kbd, \
+		&udi_api_hid_generic
+//@}
 
-#define  USB_DEVICE_EP_CTRL_SIZE       8 // control endpoint size
-#define  USB_DEVICE_NB_INTERFACE       2 // # of interfaces (1 or more)
 
-#undef USB_DEVICE_MAX_EP   // undefine this definition in header file
-#define  USB_DEVICE_MAX_EP    2	// changed from 2 -> 1 by UniWest
+/**
+ * USB Device Driver Configuration
+ * @{
+ */
+//@}
 
-/* ------------------------------------------------------------------------
- * ------------------- USB Device Driver Configuration --------------------
- * ----------------------------------------------------------------------*/
-#include "udi_composite_conf.h"
+//! The includes of classes and other headers must be done at the end of this file to avoid compile error
+
+/* Example of include for interface
+#include "udi_msc.h"
+#include "udi_hid_kbd.h"
+#include "udi_hid_mouse.h"
+#include "udi_cdc.h"
+#include "udi_phdc.h"
+#include "udi_vendor.h"
+*/
+/* Declaration of callbacks used by USB
+#include "callback_def.h"
+*/
+#include "udi_hid_kbd.h"
+#include "udi_hid_generic.h"
+
 #include "main.h"
-
-// #include "ui.h"
 #include "76319_ui.h"
 
 
